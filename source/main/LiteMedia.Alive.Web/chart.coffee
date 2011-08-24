19aae7bba8bc4e24908992a831c5958b3dd48b3e
mikael.lundin <mikael.lundin@localhost>
2011-08-24 07:49:31 +0200

    Managed to plot out a graph with canvas.

--- source/main/LiteMedia.Alive.Web/chart.coffee
+++ source/main/LiteMedia.Alive.Web/chart.coffee
@@ -1,35 +1,52 @@
-###
-  Initialize the litemedia module
-  @module litemedia
-###
-litemedia = litemedia || {}
+margin = 10
 
-###
-  Creates namespaces in the litemedia module
-  @method namespace
-  @param {String} The namespace name hiarki seperated by dot, example 'litemedia.alive.chart'
-  @return {Object} The namespace on where to place values and functions
-###
-namespace = (ns_string, module) ->
-	parts = ns_string.split('.')
-	# exit condition
-	if parts.length > 0
-		module
-	# skip 'litemedia'
-	else if parts[0] = 'litemedia'
-		namespace(parts.slice(1).join('.'))
-	# create module and recurse
-	else
-		if not module[parts[0]]?
-			module[parts[0]] = {}
-		namespace(parts.slice(1).join('.'), module[parts[0]])
+# get max value of graph, 100 is minimal
+getMax = (data) ->
+	max = 100
+	for n in data
+		max = Math.max(max, n)
+	max
 
-###
-  Chart namespace
-  @namespace litemedia.alive.chart
-###
-namespace('litemedia.alive.chart', litemedia)
+# Get the scaler of y
+yScaler = (size, max) -> (size.height - margin * 2) / max
 
-litemedia.alive.chart =
-	paint: (canvas, data) -> alert('hello')
+# Get number of pixels between discrete x
+xStep = (size, length) -> Math.round((size.width - margin) / length)
+
+# paint one graph on the canvas
+# todo, use quadratic curves?
+graph = (context, size, data, max, color) ->
+	context.beginPath()
+	context.moveTo(margin, size.height - margin) # 0, 0
+	x = margin
+	step = xStep size, data.length
+	scaler = yScaler size, 100
+	for item in data
+		x += step
+		y = (size.height - margin) - Math.round(item * scaler)
+		console.log('x:' + x + ', y:' + y)
+		context.lineTo(x, y)		
+	context.stroke()
+
+# paint grids
+grids = (context, size) ->
+	context.beginPath()
+	context.moveTo(margin, margin)
+	context.lineTo(margin, size.height - margin)
+	context.lineTo(size.width - margin, size.height - margin)
+	context.stroke()
+
+# inner function
+paintMore = (context, size, data) ->
+	grids(context, size)
+	graph(context, size, [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89], 100, '')
 	
+size = (canvas) -> width: canvas.width, height: canvas.height
+
+# Entry function for painting chart
+this.paint = (canvas, data) =>
+	context = canvas.getContext('2d')
+	if context?
+		paintMore(context, size(canvas), data)
+	else
+		# error handling
--- source/main/LiteMedia.Alive.Web/chart.js
+++ source/main/LiteMedia.Alive.Web/chart.js
@@ -1,37 +1,62 @@
 (function() {
-  /*
-    Initialize the litemedia module
-    @module litemedia
-  */  var litemedia, namespace;
-  litemedia = litemedia || {};
-  /*
-    Creates namespaces in the litemedia module
-    @method namespace
-    @param {String} The namespace name hiarki seperated by dot, example 'litemedia.alive.chart'
-    @return {Object} The namespace on where to place values and functions
-  */
-  namespace = function(ns_string, module) {
-    var parts;
-    parts = ns_string.split('.');
-    if (parts.length > 0) {
-      return module;
-    } else if (parts[0] = 'litemedia') {
-      return namespace(parts.slice(1).join('.'));
+  var getMax, graph, grids, margin, paintMore, size, xStep, yScaler;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  margin = 10;
+  getMax = function(data) {
+    var max, n, _i, _len;
+    max = 100;
+    for (_i = 0, _len = data.length; _i < _len; _i++) {
+      n = data[_i];
+      max = Math.max(max, n);
+    }
+    return max;
+  };
+  yScaler = function(size, max) {
+    return (size.height - margin * 2) / max;
+  };
+  xStep = function(size, length) {
+    return Math.round((size.width - margin) / length);
+  };
+  graph = function(context, size, data, max, color) {
+    var item, scaler, step, x, y, _i, _len;
+    context.beginPath();
+    context.moveTo(margin, size.height - margin);
+    x = margin;
+    step = xStep(size, data.length);
+    scaler = yScaler(size, 100);
+    for (_i = 0, _len = data.length; _i < _len; _i++) {
+      item = data[_i];
+      x += step;
+      y = (size.height - margin) - Math.round(item * scaler);
+      console.log('x:' + x + ', y:' + y);
+      context.lineTo(x, y);
+    }
+    return context.stroke();
+  };
+  grids = function(context, size) {
+    context.beginPath();
+    context.moveTo(margin, margin);
+    context.lineTo(margin, size.height - margin);
+    context.lineTo(size.width - margin, size.height - margin);
+    return context.stroke();
+  };
+  paintMore = function(context, size, data) {
+    grids(context, size);
+    return graph(context, size, [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89], 100, '');
+  };
+  size = function(canvas) {
+    return {
+      width: canvas.width,
+      height: canvas.height
+    };
+  };
+  this.paint = __bind(function(canvas, data) {
+    var context;
+    context = canvas.getContext('2d');
+    if (context != null) {
+      return paintMore(context, size(canvas), data);
     } else {
-      if (!(module[parts[0]] != null)) {
-        module[parts[0]] = {};
-      }
-      return namespace(parts.slice(1).join('.'), module[parts[0]]);
+      ;
     }
-  };
-  /*
-    Chart namespace
-    @namespace litemedia.alive.chart
-  */
-  namespace('litemedia.alive.chart', litemedia);
-  litemedia.alive.chart = {
-    paint: function(canvas, data) {
-      return alert('hello');
-    }
-  };
+  }, this);
 }).call(this);
