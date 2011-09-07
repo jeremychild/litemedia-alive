4a198b3d2de0d909e08dc22ffe3d722d79a26fbd
mikael.lundin <mikael.lundin@localhost>
2011-09-07 07:57:28 +0200

    Fixed packaging so the build script now zip up a complete package.

--- default.ps1
+++ default.ps1
@@ -9,6 +9,7 @@ properties {
 	
 	# Tools
 	$exe_ilmerge = $cwd + 'source\packages\ilmerge.2.10.526.4\ilmerge.exe'
+	$exe_zip = 'source\packages\7z\7z.exe'
 	
 	# Paths
 	$sys_net4 = 'C:\Windows\Microsoft.NET\Framework\v4.0.30319'
@@ -20,8 +21,12 @@ properties {
 	$dir_compile_v2 = $dir_compile + 'v2.0\'
 	$dir_compile_v4 = $dir_compile + 'v4\'
 	$dir_package = $dir_build + 'Package\'
-	$dir_package_v2 = $dir_package + 'v2.0\'
-	$dir_package_v4 = $dir_package + 'v4\'
+	$dir_package_v2 = $dir_package + 'Alive-0.1-net20\'
+	$dir_package_v2_bin = $dir_package_v2 + 'bin\'
+	$dir_package_v2_zip = $dir_package + 'Alive-0.1-net20.zip'
+	$dir_package_v4 = $dir_package + 'Alive-0.1-net4\'
+	$dir_package_v4_bin = $dir_package_v4 + 'bin\'
+	$dir_package_v4_zip = $dir_package + 'Alive-0.1-net4.zip'
 	$dir_test = $dir_build + 'Test\'
 	
 	# Source directories
@@ -48,7 +53,8 @@ task Clean {
 	md $dir_compile_v4
 	md $dir_package
 	md $dir_package_v2
-	md $dir_package_v4
+	md $dir_package_v2_bin
+	md $dir_package_v4_bin
 	
 	md $dir_test
 }
@@ -61,5 +67,14 @@ task Compile -depends Clean {
 }
 
 task Package -depends Compile {
-	#exec { &$exe_ilmerge /lib:"$sys_net4" /lib:"$sys_publicAsm" /t:dll /targetplatform:"v4,$sys_net4" /out:"$dir_package_v4\LiteMedia.Alive.dll" "$dir_compile_v4\LiteMedia.Alive.dll" "$dir_compile_v4\FSharp.Core.dll" }
+	
+	# Package version 2.0
+	exec { &$exe_ilmerge /t:dll /targetplatform:"v4" /out:"$dir_package_v2\bin\Alive.dll" "$dir_compile_v2\Alive.dll" "$dir_compile_v2\FSharp.Core.dll" }
+	Copy-Item "Documentation\*" "$dir_package_v2" -recurse
+	exec { &$exe_zip a -tzip "$dir_package_v2_zip" "$dir_package_v2"  }
+	
+	# Package version 4
+	exec { &$exe_ilmerge /lib:"$sys_net4" /lib:"$sys_publicAsm" /t:dll /targetplatform:"v4,$sys_net4" /out:"$dir_package_v4\bin\Alive.dll" "$dir_compile_v4\Alive.dll" "$dir_compile_v4\FSharp.Core.dll" }
+	Copy-Item "Documentation\*" "$dir_package_v4" -recurse
+	exec { &$exe_zip a -tzip "$dir_package_v4_zip" "$dir_package_v4"  }
 }
