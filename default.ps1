8bd687bcf3623b8efef23b6cced1b5e1cd177f5e
Mikael Lundin <mikael.lundin@litemedia.se>
2011-11-27 21:34:59 +0100

    Build script now produces both x86 and x64 versions of Alive.

--- default.ps1
+++ default.ps1
@@ -8,6 +8,8 @@ properties {
 	$cwd = [System.IO.Directory]::GetCurrentDirectory() + '\'
 	
 	# Tools
+	$exe_msbuild_x86 = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
+	$exe_msbuild_x64 = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
 	$exe_ilmerge = $cwd + 'source\packages\ilmerge.2.10.526.4\ilmerge.exe'
 	$exe_zip = 'source\packages\7z\7z.exe'
 	
@@ -24,7 +26,7 @@ properties {
 	# x86
 	$dir_compile_x86 = $dir_compile + 'x86\'
 	$dir_compile_x86_v2 = $dir_compile_x86 + 'net20\'
-	$dir_compile_x86_v4 = $dir_compile_x86 + 'v4\'
+	$dir_compile_x86_v4 = $dir_compile_x86 + 'net4\'
 	$dir_package_x86 = $dir_package + 'x86\'
 	$dir_package_x86_v2 = $dir_package_x86 + 'Alive-0.1-net20\'
 	$dir_package_x86_v2_bin = $dir_package_x86_v2 + 'bin\'
@@ -34,6 +36,16 @@ properties {
 	$dir_package_x86_v4_zip = $dir_package_x86 + 'Alive-0.1-x86-net4.zip'
 	
 	# x64
+	$dir_compile_x64 = $dir_compile + 'x64\'
+	$dir_compile_x64_v2 = $dir_compile_x64 + 'net20\'
+	$dir_compile_x64_v4 = $dir_compile_x64 + 'net4\'	
+	$dir_package_x64 = $dir_package + 'x64\'
+	$dir_package_x64_v2 = $dir_package_x64 + 'Alive-0.1-net20\'
+	$dir_package_x64_v2_bin = $dir_package_x64_v2 + 'bin\'
+	$dir_package_x64_v2_zip = $dir_package_x64 + 'Alive-0.1-x64-net20.zip'
+	$dir_package_x64_v4 = $dir_package_x64 + 'Alive-0.1-net4\'
+	$dir_package_x64_v4_bin = $dir_package_x64_v4 + 'bin\'
+	$dir_package_x64_v4_zip = $dir_package_x64 + 'Alive-0.1-x64-net4.zip'
 	
 	# Source directories
 	$dir_source = $cwd + 'Source\'
@@ -52,30 +64,50 @@ task Clean {
 		rd $dir_build -Recurse -Force
 	}
 	
+	
+	
 	# Recreate build target directories
-	md $dir_build 
+	md $dir_build
+	md $dir_package
+	md $dir_test
+	
+	# x86
 	md $dir_compile_x86
 	md $dir_compile_x86_v2
 	md $dir_compile_x86_v4
-	md $dir_package
 	md $dir_package_x86
 	md $dir_package_x86_v2
 	md $dir_package_x86_v2_bin
 	md $dir_package_x86_v4
 	md $dir_package_x86_v4_bin
 	
-	md $dir_test
+	# x64
+	md $dir_compile_x64
+	md $dir_compile_x64_v2
+	md $dir_compile_x64_v4
+	md $dir_package_x64
+	md $dir_package_x64_v2
+	md $dir_package_x64_v2_bin
+	md $dir_package_x64_v4
+	md $dir_package_x64_v4_bin
 }
 
 # Compile the project
 task Compile -depends Clean {
-
 	# x86
 	# Compile .NET version 2.0
-	exec { msbuild $lib_fsproj_v2 /p:OutputPath="$dir_compile_x86_v2" /p:SolutionDir="$dir_source" }	
+	exec { & $exe_msbuild_x86 $lib_fsproj_v2 "/p:OutputPath=$dir_compile_x86_v2;SolutionDir=$dir_source;Configuration=Release;Platform=x86" }
 
 	# Compile .NET version 4.0
-	exec { msbuild $lib_fsproj_v4 /verbosity:minimal /p:Configuration="$configuration" /p:Platform="Any CPU" /p:OutDir="$dir_compile_x86_v4" /p:OutputPath="$dir_compile_x86_v4" /p:SolutionDir="$dir_source" }	
+	exec { & $exe_msbuild_x86 $lib_fsproj_v4 "/p:OutputPath=$dir_compile_x86_v4;SolutionDir=$dir_source;Configuration=Release;Platform=x86" }
+	
+	
+	# x64
+	# Compile .NET version 2.0
+	exec { & $exe_msbuild_x86 $lib_fsproj_v2 "/p:OutputPath=$dir_compile_x64_v2;SolutionDir=$dir_source;Configuration=Release;Platform=x64" }
+
+	# Compile .NET version 4.0
+	exec { & $exe_msbuild_x86 $lib_fsproj_v4 "/p:OutputPath=$dir_compile_x64_v4;SolutionDir=$dir_source;Configuration=Release;Platform=x64" }	
 }
 
 task Package -depends Compile {
@@ -90,4 +122,15 @@ task Package -depends Compile {
 	exec { &$exe_ilmerge /lib:"$sys_net4" /lib:"$sys_publicAsm" /t:dll /targetplatform:"v4,$sys_net4" /out:"$dir_package_x86_v4\bin\Alive.dll" "$dir_compile_x86_v4\Alive.dll" "$dir_compile_x86_v4\FSharp.Core.dll" }
 	Copy-Item "Documentation\*" "$dir_package_x86_v4" -recurse
 	exec { &$exe_zip a -tzip "$dir_package_x86_v4_zip" "$dir_package_x86_v4"  }
+	
+	# x64
+	# Package version 2.0
+	exec { &$exe_ilmerge /t:dll /out:"$dir_package_x64_v2\bin\Alive.dll" "$dir_compile_x64_v2\Alive.dll" "$dir_compile_x64_v2\FSharp.Core.dll" }
+	Copy-Item "Documentation\*" "$dir_package_x64_v2" -recurse
+	exec { &$exe_zip a -tzip "$dir_package_x64_v2_zip" "$dir_package_x64_v2"  }
+	
+	# Package version 4
+	exec { &$exe_ilmerge /lib:"$sys_net4" /lib:"$sys_publicAsm" /t:dll /targetplatform:"v4,$sys_net4" /out:"$dir_package_x64_v4\bin\Alive.dll" "$dir_compile_x64_v4\Alive.dll" "$dir_compile_x64_v4\FSharp.Core.dll" }
+	Copy-Item "Documentation\*" "$dir_package_x64_v4" -recurse
+	exec { &$exe_zip a -tzip "$dir_package_x64_v4_zip" "$dir_package_x64_v4"  }
 }
