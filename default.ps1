121d8c355f704b81c3d68e4b960a24e84d3dff27
mikael.lundin <mikael.lundin@localhost>
2011-11-29 16:51:19 +0100

    Working on NuGet packaging.

--- default.ps1
+++ default.ps1
@@ -15,6 +15,8 @@ properties {
 	
 	# Paths
 	$sys_net4 = 'C:\Windows\Microsoft.NET\Framework\v4.0.30319'
+	$sys_net30 = 'C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\v3.0'
+	$sys_net35 = 'C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\v3.5'
 	$sys_publicAsm = 'C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\PublicAssemblies'
 	
 	# Directories we use for the build
@@ -47,8 +49,15 @@ properties {
 	$dir_package_x64_v4_bin = $dir_package_x64_v4 + 'bin\'
 	$dir_package_x64_v4_zip = $dir_package_x64 + 'Alive-0.1-x64-net4.zip'
 	
+	# nuget
+	$dir_nuget = $dir_build + 'nuget\'
+	$dir_nuget_lib = $dir_nuget + 'lib\'
+	$dir_nuget_lib_v2 = $dir_nuget_lib + 'net20\'
+	$dir_nuget_lib_v4 = $dir_nuget_lib + 'net40\'
+	
 	# Source directories
 	$dir_source = $cwd + 'Source\'
+	$dir_config = $dir_source + 'configuration\'
 	$dir_lib = $dir_source + 'main\LiteMedia.Alive\'
 	
 	# Project files
@@ -59,37 +68,28 @@ properties {
 task default -depends Compile
 
 task Clean {
+	# Clean msbuild directories within projects
+	exec { msbuild "$dir_source\LiteMedia.Alive.sln" /t:Clean "/p:SolutionDir=$dir_source;Configuration=Release;Platform=x86" }
+	exec { msbuild "$dir_source\LiteMedia.Alive.sln" /t:Clean "/p:SolutionDir=$dir_source;Configuration=Release;Platform=x64" }
+
 	# If build dir exists, delete it
 	if (Test-Path $dir_build) {
 		rd $dir_build -Recurse -Force
 	}
 	
-	
-	
 	# Recreate build target directories
-	md $dir_build
-	md $dir_package
-	md $dir_test
+	@($dir_build, $dir_package, $dir_nuget, $dir_test) | ForEach-Object { New-Item $_ -Type d }
 	
-	# x86
-	md $dir_compile_x86
-	md $dir_compile_x86_v2
-	md $dir_compile_x86_v4
-	md $dir_package_x86
-	md $dir_package_x86_v2
-	md $dir_package_x86_v2_bin
-	md $dir_package_x86_v4
-	md $dir_package_x86_v4_bin
+	# Recreate x86 directories
+	@($dir_compile_x86, $dir_compile_x86_v2, $dir_compile_x86_v4)  | ForEach-Object { New-Item $_ -Type d }
+	@($dir_package_x86, $dir_package_x86_v2, $dir_package_x86_v2_bin, $dir_package_x86_v4, $dir_package_x86_v4_bin) | ForEach-Object { New-Item $_ -Type d }
 	
-	# x64
-	md $dir_compile_x64
-	md $dir_compile_x64_v2
-	md $dir_compile_x64_v4
-	md $dir_package_x64
-	md $dir_package_x64_v2
-	md $dir_package_x64_v2_bin
-	md $dir_package_x64_v4
-	md $dir_package_x64_v4_bin
+	# Recreate x64 directories
+	@($dir_compile_x64, $dir_compile_x64_v2, $dir_compile_x64_v4) | foreach-object -process { New-Item $_ -Type d }
+	@($dir_package_x64, $dir_package_x64_v2, $dir_package_x64_v2_bin, $dir_package_x64_v4, $dir_package_x64_v4_bin) | ForEach-Object { New-Item $_ -Type d }
+	
+	# Recreate nuget directory structure
+	@($dir_nuget_lib, $dir_nuget_lib_v2, $dir_nuget_lib_v4) | ForEach-Object { New-Item $_ -Type d }
 }
 
 # Compile the project
@@ -111,26 +111,33 @@ task Compile -depends Clean {
 }
 
 task Package -depends Compile {
-
+	# ilmerge
 	# x86
-	# Package version 2.0
-	exec { &$exe_ilmerge /t:dll /out:"$dir_package_x86_v2\bin\Alive.dll" "$dir_compile_x86_v2\Alive.dll" "$dir_compile_x86_v2\FSharp.Core.dll" }
-	Copy-Item "Documentation\*" "$dir_package_x86_v2" -recurse
-	exec { &$exe_zip a -tzip "$dir_package_x86_v2_zip" "$dir_package_x86_v2"  }
-	
-	# Package version 4
+	Write-Host "ILMerge x86 v2.0"
+	exec { &$exe_ilmerge /targetplatform:"v2,C:\Windows\Microsoft.NET\Framework\v2.0.50727" /t:dll /out:"$dir_package_x86_v2\bin\Alive.dll" "$dir_compile_x86_v2\Alive.dll" "$dir_compile_x86_v2\FSharp.Core.dll" }
+	Write-Host "ILMerge x86 v4"
 	exec { &$exe_ilmerge /lib:"$sys_net4" /lib:"$sys_publicAsm" /t:dll /targetplatform:"v4,$sys_net4" /out:"$dir_package_x86_v4\bin\Alive.dll" "$dir_compile_x86_v4\Alive.dll" "$dir_compile_x86_v4\FSharp.Core.dll" }
+	# x64
+	Write-Host "ILMerge x64 v2.0"
+	exec { &$exe_ilmerge /t:dll /out:"$dir_package_x64_v2\bin\Alive.dll" "$dir_compile_x64_v2\Alive.dll" "$dir_compile_x64_v2\FSharp.Core.dll" }
+	Write-Host "ILMerge x64 v4"
+	exec { &$exe_ilmerge /lib:"$sys_net4" /lib:"$sys_publicAsm" /t:dll /targetplatform:"v4,$sys_net4" /out:"$dir_package_x64_v4\bin\Alive.dll" "$dir_compile_x64_v4\Alive.dll" "$dir_compile_x64_v4\FSharp.Core.dll" }
+	
+	# create nuget package
+	Copy-Item "$dir_package_x64_v2\bin\Alive.dll" $dir_nuget_lib_v2
+	Copy-Item "$dir_package_x64_v4\bin\Alive.dll" $dir_nuget_lib_v4
+	Copy-Item "$dir_config\nuget\content" $dir_nuget
+
+	# create zip package
+	# x86
+	Copy-Item "Documentation\*" "$dir_package_x86_v2" -recurse
+	exec { &$exe_zip a -tzip "$dir_package_x86_v2_zip" "$dir_package_x86_v2"  }	
 	Copy-Item "Documentation\*" "$dir_package_x86_v4" -recurse
 	exec { &$exe_zip a -tzip "$dir_package_x86_v4_zip" "$dir_package_x86_v4"  }
 	
 	# x64
-	# Package version 2.0
-	exec { &$exe_ilmerge /t:dll /out:"$dir_package_x64_v2\bin\Alive.dll" "$dir_compile_x64_v2\Alive.dll" "$dir_compile_x64_v2\FSharp.Core.dll" }
 	Copy-Item "Documentation\*" "$dir_package_x64_v2" -recurse
 	exec { &$exe_zip a -tzip "$dir_package_x64_v2_zip" "$dir_package_x64_v2"  }
-	
-	# Package version 4
-	exec { &$exe_ilmerge /lib:"$sys_net4" /lib:"$sys_publicAsm" /t:dll /targetplatform:"v4,$sys_net4" /out:"$dir_package_x64_v4\bin\Alive.dll" "$dir_compile_x64_v4\Alive.dll" "$dir_compile_x64_v4\FSharp.Core.dll" }
 	Copy-Item "Documentation\*" "$dir_package_x64_v4" -recurse
 	exec { &$exe_zip a -tzip "$dir_package_x64_v4_zip" "$dir_package_x64_v4"  }
 }
